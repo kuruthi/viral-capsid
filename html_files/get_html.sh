2572b435a331d618b5f7b332d50c7aeb83289856
P venkateshwar <venkateshwarp81@gmail.com>
2022-02-18 18:29:23 +0530

    My first commit, just bringing the files over

---
+++ html_files/get_html.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ ### Just for documentation
+
+#Takes the txt file with the urls as the argument
+
+set -e
+
+wget -i $1
---
+++ metagenome.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(rvest) ## for possibly scraping the web pages to directly get the json data
+library(jsonlite) ## helped me import the json file into a dataframe ## the only one that worked for me
+
+setwd("~/metagenome_project/json_files")
+### Importing the pangong tso species nested list as a tibble
+
+pangong_data <- fromJSON(txt = "pangong_sed_1.json", simplifyDataFrame = TRUE)
+colnames(pangong_data) <- c("Id", "Parent","Name","Total_count","Percent","kbp","c")
+pangong_tibble <- as_tibble(pangong_data)
+pangong_tibble$Parent
+View(pangong_tibble)
